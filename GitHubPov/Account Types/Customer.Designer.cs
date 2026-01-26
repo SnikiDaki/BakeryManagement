5500767ebfeb3ca31820eb5a551cc7884998f0b7
d3ky1 <david.obuch.do@gmail.com>
2026-01-26 19:52:42 +0100

    Version 0.6 - Customer Design

--- GitHubPov/Account Types/Customer.Designer.cs
+++ GitHubPov/Account Types/Customer.Designer.cs
@@ -28,12 +28,351 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Customer";
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
+            numericUpDown5 = new NumericUpDown();
+            numericUpDown6 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(728, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Username";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(32, 50);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(147, 101);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(263, 50);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(147, 101);
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(483, 50);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(147, 101);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(32, 219);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(147, 101);
+            pictureBox4.TabIndex = 4;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(263, 219);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(147, 101);
+            pictureBox5.TabIndex = 5;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(483, 219);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(147, 101);
+            pictureBox6.TabIndex = 6;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(692, 12);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(30, 31);
+            pictureBox7.TabIndex = 7;
+            pictureBox7.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 335);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 8;
+            label2.Text = "Torta 4";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(32, 165);
+            label3.Name = "label3";
+            label3.Size = new Size(43, 15);
+            label3.TabIndex = 9;
+            label3.Text = "Torta 1";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(263, 165);
+            label4.Name = "label4";
+            label4.Size = new Size(43, 15);
+            label4.TabIndex = 10;
+            label4.Text = "Torta 2";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(483, 165);
+            label5.Name = "label5";
+            label5.Size = new Size(43, 15);
+            label5.TabIndex = 11;
+            label5.Text = "Torta 3";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(263, 335);
+            label6.Name = "label6";
+            label6.Size = new Size(43, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Torta 5";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(483, 335);
+            label7.Name = "label7";
+            label7.Size = new Size(43, 15);
+            label7.TabIndex = 13;
+            label7.Text = "Torta 6";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(483, 363);
+            label8.Name = "label8";
+            label8.Size = new Size(56, 15);
+            label8.TabIndex = 14;
+            label8.Text = "Quantity:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(263, 363);
+            label9.Name = "label9";
+            label9.Size = new Size(56, 15);
+            label9.TabIndex = 15;
+            label9.Text = "Quantity:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(32, 363);
+            label10.Name = "label10";
+            label10.Size = new Size(56, 15);
+            label10.TabIndex = 16;
+            label10.Text = "Quantity:";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(32, 189);
+            label11.Name = "label11";
+            label11.Size = new Size(56, 15);
+            label11.TabIndex = 17;
+            label11.Text = "Quantity:";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(263, 189);
+            label12.Name = "label12";
+            label12.Size = new Size(56, 15);
+            label12.TabIndex = 18;
+            label12.Text = "Quantity:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(483, 189);
+            label13.Name = "label13";
+            label13.Size = new Size(56, 15);
+            label13.TabIndex = 19;
+            label13.Text = "Quantity:";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(692, 391);
+            button1.Name = "button1";
+            button1.Size = new Size(89, 38);
+            button1.TabIndex = 20;
+            button1.Text = "Order";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(574, 355);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(56, 23);
+            numericUpDown1.TabIndex = 21;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(574, 181);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(56, 23);
+            numericUpDown2.TabIndex = 22;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(354, 181);
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(56, 23);
+            numericUpDown3.TabIndex = 23;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(123, 181);
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(56, 23);
+            numericUpDown4.TabIndex = 24;
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Location = new Point(123, 355);
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(56, 23);
+            numericUpDown5.TabIndex = 25;
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.Location = new Point(354, 355);
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(56, 23);
+            numericUpDown6.TabIndex = 26;
+            // 
+            // Customer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(numericUpDown6);
+            Controls.Add(numericUpDown5);
+            Controls.Add(numericUpDown4);
+            Controls.Add(numericUpDown3);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Name = "Customer";
+            Text = "Customer";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown4;
+        private NumericUpDown numericUpDown5;
+        private NumericUpDown numericUpDown6;
     }
 }
