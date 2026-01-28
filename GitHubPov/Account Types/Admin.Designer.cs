f1793d78741aa80d85b2937ef365a24f8a100b08
Sinishaa <craftinglgm4u@gmail.com>
2026-01-28 07:59:28 +0100

    - Changed admin menu

--- GitHubPov/Account Types/Admin.Designer.cs
+++ GitHubPov/Account Types/Admin.Designer.cs
@@ -29,12 +29,15 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            button1.Location = new Point(49, 143);
+            button1.Location = new Point(42, 153);
             button1.Name = "button1";
             button1.Size = new Size(170, 120);
             button1.TabIndex = 0;
@@ -42,11 +45,44 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
+            // button2
+            // 
+            button2.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(227, 153);
+            button2.Name = "button2";
+            button2.Size = new Size(170, 120);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(412, 153);
+            button3.Name = "button3";
+            button3.Size = new Size(170, 120);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Arial", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(597, 153);
+            button4.Name = "button4";
+            button4.Size = new Size(170, 120);
+            button4.TabIndex = 3;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
             // Admin
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
             Controls.Add(button1);
             Name = "Admin";
             Text = "Admin";
@@ -56,5 +92,8 @@
         #endregion
 
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
